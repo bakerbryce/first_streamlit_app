e1ac7d6a803bb984aa6fd739441497fa394bf474
Bryce Baker <36087020+bakerbryce@users.noreply.github.com>
2022-10-20 13:20:30 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,18 @@
 import streamlit as sl
+import pandas as pd
 
 sl.title('My Parents New Healthy Diner')
 
 sl.header('Breakfast Menu')
-sl.text('🥣Omega 3 & Blueberry Oatmeal')
+sl.text('🥣 Omega 3 & Blueberry Oatmeal')
 sl.text('🥗 Kale, Spinach & Rocket Smoothie')
 sl.text('🐔 Hard-Boiled Free-Range Egg')
 sl.text('🥑🍞 Avocado Toast')
+
+sl.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+# Get fruit list from S3
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+# Display the fruit list
+sl.dataframe(my_fruit_list)
