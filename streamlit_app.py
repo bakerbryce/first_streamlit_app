b511c8e7fa33f3a1ca4b2cb5d496fea14022a3ca
Bryce Baker <36087020+bakerbryce@users.noreply.github.com>
2022-10-20 13:34:29 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,10 @@ sl.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 # Get fruit list from S3
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
-# Display the fruit list
+# Pick list of fruits
+sl.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
+# Display the list
 sl.dataframe(my_fruit_list)
